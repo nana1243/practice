a48194462496e7814b13c5466a3922a97ac2e638
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-14 07:34:09 +0900

    fea)[7/14] 초기 계산기 프로젝트 셋팅

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,11 +1,10 @@
+import Calculator from "./components/Calculator";
+
 function App() {
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-            안녕하세요! Hello
-        </h1>
-
+        <Calculator/>
     </>
   )
 }
