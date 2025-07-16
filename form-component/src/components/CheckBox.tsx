200ad2ad1e9e001ab45521e0a6372011c512c069
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-17 07:20:47 +0900

    fea)[7/17] formState로 한번에 상태값 제어하기

--- form-component/src/components/CheckBox.tsx
+++ form-component/src/components/CheckBox.tsx
@@ -1,16 +1,40 @@
 import React, {useState} from 'react';
 
 function CheckBox() {
-    const [checked, setChecked] = useState(false);
+    const [checked1, setChecked1] = useState(false);
+    const [checked2, setChecked2] = useState(false);
+    const [formState, setFormState] = useState({
+        checked1: false,
+        checked2: false
+    });
+
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
+        setChecked1(event.target.checked);
     };
+    const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked2(event.target.checked);
+    }
+
+    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = event.target;
+        console.log('name', name)
+        console.log('checked', checked)
+        setFormState(prevState => ({
+            ...prevState,
+            [name]: checked
+        }));
+    }
 
     return (
         <div>
-            <input type="checkbox" id="item1" checked={checked} onChange={handleChange} />
-            <label htmlFor="item1">Item 1 : check ? :{String(checked)}</label>
+            <form >
+                <input type="checkbox" name="checked1" id="item1" checked={formState.checked1} onChange={handleFormChange} />
+                <label htmlFor="item1">Item 1 : check ? :{String(formState.checked1)}</label>
+
+                <input type="checkbox" id="item2" name="checked2" checked={formState.checked2} onChange={handleFormChange} />
+                <label htmlFor="item2">Item 2 : check ? :{String(formState.checked2)}</label>
+            </form>
         </div>
     );
 }
