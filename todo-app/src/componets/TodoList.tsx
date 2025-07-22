1468edeea896c3142f8da3eb43d5eee9752d4af9
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-23 07:27:56 +0900

    fea)[7/23] 컴포넌트 분리

--- todo-app/src/componets/TodoList.tsx
+++ todo-app/src/componets/TodoList.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
+import CheckBox from "./html/CheckBox";
+import Button from "./html/Button";
 import SvgPencil from "./svg/SvgPencil";
 import SvgClose from "./svg/SvgClose";
+import Input from "./html/Input";
 
 function TodoList(props) {
     return (
-        <div className="todo">
-            <h1 className="todo__title">Todo List</h1>
-            <p className="todo__subtitle">Please enter your details to continue.</p>
-
-            <form className="todo__form">
-                <div className="todo__editor">
-                    <input
-                        type="text"
-                        className="todo__input"
-                        placeholder="Enter Todo List"
-                    />
-                    <button className="todo__button" type="submit">Add</button>
-                </div>
-            </form>
-
-
+        <>
             <ul className="todo__list">
                 {/* 할 일 목록이 없을 때 */}
                 <li className="todo__item todo__item--empty">
@@ -28,19 +16,23 @@ function TodoList(props) {
                 {/*<!-- 할 일 목록이 있을 때 -->*/}
                 {/*{!--할 일이 완료되면 .todo__item--complete 추가 -->}*/}
                 <li className="todo__item todo__item--complete">
-                    <div className="todo__checkbox-group">
-                        <input type="checkbox" className="todo__checkbox" checked/>
-                        <label>Eat Breakfast</label>
-                    </div>
+                    <CheckBox
+                        parentClassName="todo__checkbox-group"
+                        type="checkbox"
+                        className="todo__checkbox"
+                        defaultChecked
+                    >
+                        Eat Breakfast
+                    </CheckBox>
                     {/*{<!-- 할 일을 수정할 때만 노출 (.todo__checkbox-group은 비노출) -->}*/}
                     {/*{<!-- <input type="text" class="todo__modify-input" /> -->}*/}
                     <div className="todo__button-group">
-                        <button className="todo__action-button">
+                        <Button className="todo__action-button">
                             <SvgPencil/>
-                        </button>
-                        <button className="todo__action-button">
+                        </Button>
+                        <Button className="todo__action-button">
                             <SvgClose/>
-                        </button>
+                        </Button>
                     </div>
                 </li>
                 <li className="todo__item todo__item--complete">
@@ -49,18 +41,19 @@ function TodoList(props) {
                     {/*    <label>Eat Breakfast</label>*/}
                     {/*    </div> -->*/}
                     {/*    <!-- 할 일을 수정할 때만 노출 (.todo__checkbox-group은 비노출) -->}                    */}
-                    <input type="text" className="todo__modify-input"/>
+                    <Input type="text" className="todo__modify-input"/>
                     <div className="todo__button-group">
-                        <button className="todo__action-button">
+                        <Button className="todo__action-button">
                             <SvgPencil/>
-                        </button>
-                        <button className="todo__action-button">
+                        </Button>
+                        <Button className="todo__action-button">
                             <SvgClose/>
-                        </button>
+                        </Button>
                     </div>
                 </li>
             </ul>
-        </div>
+
+        </>
     );
 }
 
