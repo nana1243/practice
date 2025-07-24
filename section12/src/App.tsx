532b6c52e1929bcabd333ba126e78bf13197f1b7
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-25 07:15:11 +0900

    fea)[7/25] useEffect 훅 사용

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css'
 import CheckBox from "./components/CheckBox";
+import {useEffect, useState} from "react";
 
 function App() {
+    const  [count,setCount] = useState(0);
 
-  return (
-    <>
-      <CheckBox/>
-      <CheckBox/>
-    </>
+    useEffect(()=> {
+        console.log('App mounted');
+
+        // Cleanup function
+        return () => {
+            console.log('App unmounted');
+        }
+    },[])
+
+    useEffect(() => {
+        console.log('Count changed:', count);
+    }, [count]);
+
+
+    return (
+        <>
+            <h1>Count : {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Click Me</button>
+        </>
   )
 }
 
