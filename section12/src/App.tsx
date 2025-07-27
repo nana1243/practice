2c0b783d2b5f67c2fd8edafa831c3847607fb5cf
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-27 11:34:19 +0900

    fea)[7/27] 값을 메모이제이션 할때 useMemo훅으로

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
-import CheckBox from "./components/CheckBox";
-import {useCallback, useEffect, useState} from "react";
-import ChildA from "./components/ChildA";
+import {useCallback, useEffect, useMemo, useState} from "react";
+// import ChildA from "./components/ChildA";
+
+const initialItems = new Array(29_999_999).fill(0).map((_, index) => {
+    return {
+        id: index,
+        selected: index ===29_999_998
+    };
+});
 
 function App() {
     const  [count,setCount] = useState(0);
+
+    // 연산비용이 높음
+    // const selectedItems = initialItems.find(item => item.selected);
+
+    const selectedItems = useMemo(() => initialItems.find(item=> item.selected), []);
+
 
     useEffect(()=> {
         console.log('App mounted');
@@ -36,7 +48,8 @@ function App() {
         <>
             <h1>Count : {count}</h1>
             <button onClick={incrementCountV2}>Click Me</button>
-            <ChildA increment={incrementCountV2}/>
+            <p>selected : {selectedItems.id}</p>
+            {/*<ChildA increment={incrementCountV2}/>*/}
         </>
   )
 }
