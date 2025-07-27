869ab2af76c1c7d1d1b4db953d4dc523c03386e2
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-27 10:54:40 +0900

    fea)[7/27] props 변경시 리렌더링

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <>
             <h1>Count : {count}</h1>
             <button onClick={() => setCount(count + 1)}>Click Me</button>
-            <ChildA/>
+            <ChildA count={count}/>
         </>
   )
 }
--- section12/src/components/ChildA.tsx
+++ section12/src/components/ChildA.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ChildB from "./ChildB";
 
-function ChildA(props) {
+interface ChildAProps {
+    count: number;
+}
+
+function ChildA(props :ChildAProps) {
+    const { count } = props;
     console.log('Child A rendered');
+    console.log('Child A props:', count);
     return (
         <>
             <h1>Child A</h1>
