50c9ac812b48636fc868f99eb0bdaaa0b08125e5
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-29 07:14:57 +0900

    fea)[7/29] persist 미들웨어를 통한 상태값 유

--- section15/src/store/countStore.ts
+++ section15/src/store/countStore.ts
@@ -1,4 +1,5 @@
 import {create} from "zustand";
+import {createJSONStorage, persist} from "zustand/middleware";
 
 interface CountStore{
     count: number;
@@ -7,7 +8,9 @@ interface CountStore{
     reset: () => void;
 }
 
-export const useCountStore = create<CountStore>((set,get) => ({
+export const useCountStore = create<CountStore>(
+    persist(
+        (set) => ({
     count : 0,
     decrement: async () => {
         await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate async operation
@@ -16,4 +19,7 @@ export const useCountStore = create<CountStore>((set,get) => ({
     },
     increment: (amount) => set((state) => ({count: state.count + amount })),
     reset: () => set(() => ({count: 0})),
-}));
+})),{
+    name: 'count-storage', // unique name for the storage
+    storage: createJSONStorage(()=> sessionStorage), // use sessionStorage for persistence
+    });
