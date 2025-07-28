b9ac8f5b855eb2fdabd2d7669b00a472d0e712f9
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-29 07:29:21 +0900

    fea)[7/29] subscribeWithSelector 미들웨어 사용(특정 상태를 구독해서, 상태가 변경이 될때마다 특정 로직을 사용할 수 있도록)

--- section15/src/componets/Count.tsx
+++ section15/src/componets/Count.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import CountGroup from "./CountGroup";
+import {useCountStore} from "../store/countStore";
 
 function Count(props) {
+
+    useEffect(()=>{
+        const unsubscribe = useCountStore.subscribe((state)=> state.count,
+            (newCount) => console.log("Count changed!!!! :", newCount)
+        );
+        return () => {
+            unsubscribe();
+        };
+    },[])
+
+
     return (
         <>
             <h1>Count</h1>
--- section15/src/store/countStore.ts
+++ section15/src/store/countStore.ts
@@ -1,5 +1,5 @@
 import {create} from "zustand";
-import {createJSONStorage, persist} from "zustand/middleware";
+import {createJSONStorage, persist, subscribeWithSelector} from "zustand/middleware";
 
 interface CountStore{
     count: number;
@@ -9,17 +9,22 @@ interface CountStore{
 }
 
 export const useCountStore = create<CountStore>(
-    persist(
-        (set) => ({
-    count : 0,
-    decrement: async () => {
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate async operation
-        console.log('Decrementing count...')
-        set((state => ({count: state.count - 1})));
-    },
-    increment: (amount) => set((state) => ({count: state.count + amount })),
-    reset: () => set(() => ({count: 0})),
-})),{
-    name: 'count-storage', // unique name for the storage
-    storage: createJSONStorage(()=> sessionStorage), // use sessionStorage for persistence
-    });
+    subscribeWithSelector(
+        persist(
+            (set) => ({
+                count: 0,
+                decrement: async () => {
+                    await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate async operation
+                    console.log('Decrementing count...');
+                    set((state) => ({ count: state.count - 1 }));
+                },
+                increment: (amount) => set((state) => ({ count: state.count + amount })),
+                reset: () => set(() => ({ count: 0 })),
+            }),
+            {
+                name: 'count-storage', // unique name for the storage
+                storage: createJSONStorage(() => sessionStorage), // use sessionStorage for persistence
+            }
+        )
+    )
+);
