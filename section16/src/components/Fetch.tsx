f5d1cd5718224cf3dff24788a946343e9cd49a48
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-31 07:33:51 +0900

    fea)[7/31] fetch-loading 처리

--- section16/src/components/Fetch.tsx
+++ section16/src/components/Fetch.tsx
@@ -1,14 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+
+interface Post {
+    id: number;
+    title : string;
+    views : number;
+}
 
 function Fetch() {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
     useEffect(() => {
-        fetch('http://localhost:3000/posts').then(r => console.log(r))
+        setIsLoading(true);
+        fetch('http://localhost:3000/posts')
+            .then(result => result.json())
+            .then(data=>setPosts(data as Post[]))
+            .finally(() => setIsLoading(false))
 
     },[])
 
     return (
         <>
             <h1>Fetch</h1>
+            {isLoading && <p>Loading...</p>}
+            {posts.map(post => (
+                <>
+                    <h2 key={post.id}>{post.title}</h2>
+                    <p>Views: {post.views}</p>
+                </>
+            ))}
         </>
     );
 }
