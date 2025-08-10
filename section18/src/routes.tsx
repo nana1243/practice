7685b423d81cb4e8fbbb366c8a6cec16555c5bf9
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-08-11 07:28:55 +0900

    fea)[8/11] 라우터 객체의 속성중 Component/element를 사용해서 페이지 렌더링

--- section18/src/routes.tsx
+++ section18/src/routes.tsx
@@ -5,15 +5,19 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+/*
+* 라우터 객체 : createBrowserRouter를 통해 생성된 라우터 인스턴스 객체
+* 라우트 객체 : 각 경로에 대한 컴포넌트 매핑된 객체
+* */
 const router = createBrowserRouter([
     {
         path: "/",
-        element:<Home/>,
+        Component: Home,
     },
     {
         path: "/about",
-        element: <About/>,
-    }
+        Component: About,
+    },
 ]);
 
 
