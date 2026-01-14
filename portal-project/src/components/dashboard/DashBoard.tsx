006b640f88eed80c43e36a01cc2d19af089d1dbd
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-01-15 07:57:12 +0900

    fea)[1/15] createPortal 적용시 이벤트 버블링 확인

--- portal-project/src/components/dashboard/DashBoard.tsx
+++ portal-project/src/components/dashboard/DashBoard.tsx
@@ -5,19 +5,28 @@ const DashBoard = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleClick = () => {
-        setIsModalOpen(true);
+        console.log('부모컴포넌트에서 클릭 이벤트 감지!')
     };
 
 
     return (
         <>
             <h2>Dash Board Component</h2>
-            <button onClick={handleClick}>SubMit</button>
-            <Modal
-                description="This is a modal dialog"
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-            />
+            <div onClick={handleClick} style={{border: '1px solid red'}}>
+                <button onClick={(e) => {
+                    // e.stopPropagation();
+                    setIsModalOpen(true);
+                }}>Open Modal
+                </button>
+                <Modal
+
+                    description="This is a modal dialog"
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                >
+                    <button>나를 클릭해봐!</button>
+                </Modal>
+            </div>
         </>
     )
 }
--- portal-project/src/components/modal-portal/Modal.tsx
+++ portal-project/src/components/modal-portal/Modal.tsx
@@ -16,7 +16,7 @@ const Modal = ({description, isOpen, onClose, children}) => {
     return createPortal(
         <div className={styles.modalOverlay} onClick={onClose}>
             {/* e.stopPropagation()은 모달 내부 클릭 시 닫히지 않게 함 */}
-            <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+            <div className={styles.modalContent}>
                 <div className={styles.modalHeader}>ModalTitle</div>
                 <div className={styles.modalBody}>{children ?? description}</div>
                 <div className={styles.modalFooter}>
